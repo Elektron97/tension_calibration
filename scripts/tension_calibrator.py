edcd490524631ac8287d8e3f508271da35d261ab
Elektron97 <danele.caradonna@gmail.com>
2023-11-27 23:38:47 +0100

    draw motors

--- scripts/tension_calibrator.py
+++ scripts/tension_calibrator.py
@@ -7,7 +7,7 @@ N_MOTORS = 7
 
 ## Main ##
 def main():
-    rospy.init_node("tension_calibrator_node")
+    rospy.init_node("tension_calibrator_node", anonymous=True)
     Motor_Calibrator(N_MOTORS)
     rospy.spin()
 
--- src/tension_calibration/motor_calibrator.py
+++ src/tension_calibration/motor_calibrator.py
@@ -8,6 +8,7 @@
 #If the n° of turns is equal to -1, it does means to turn off the motor.    #
 #############################################################################
 
+import numpy as np
 import rospy
 from std_msgs.msg import Float32MultiArray
 
@@ -26,8 +27,11 @@ class Motor_Calibrator:
         self.timer_obj = rospy.Timer(rospy.Duration(1/NODE_FREQUENCY), self.main_loop)
 
         # Subscriber/Publisher
-        self.pub_obj = rospy.Publisher(turns_topic_name, Float32MultiArray, QUEUE_SIZE)
+        self.pub_obj = rospy.Publisher(turns_topic_name, Float32MultiArray, queue_size=QUEUE_SIZE)
         self.sub_obj = rospy.Subscriber(current_topic_name, Float32MultiArray, self.currents_callback)
+
+        # Shutdown Callback
+        rospy.on_shutdown(self.shutdown_callback)
         
         # Init n° of motors
         self.n_motors = n_motors
@@ -42,31 +46,55 @@ class Motor_Calibrator:
 
         # Actual Commanded Turns
         self.cmd_turns = Float32MultiArray()
-        self.cmd_turns.data = [0.0]*n_motors    # Init to zeros
+        self.cmd_turns.data = [DISABLE_TORQUE_REQUEST]*self.n_motors    # Init to zeros
+
+        # To Calibrate Motors Queue
+        self.uncalibrated_motors = [*range(self.n_motors)]
+        self.calibrated_motors = []
+
+        self.draw_motor_from_queue()
     
     def currents_callback(self, msg):
         # Storing currents in the private attribute
         self.read_currents = msg
+    
+    def shutdown_callback(self):
+        rospy.logwarn("Calibration terminated. Killing the node and turn off the motors.")
+        self.cmd_turns.data = [DISABLE_TORQUE_REQUEST]*self.n_motors
+        self.publish_turns()
 
     def calibration_single_motor(self, motor_id):
         for i in range(self.n_motors):
             # Compute the commanded turns for the motor_id-th motor
             if (i == motor_id):
-                if (self.cmd_turns >= 0) and (self.cmd_turns < self.max_turns):  # [0, MAX_TURNS]
+                if (self.cmd_turns.data[i] >= 0) and (self.cmd_turns.data[i] < self.max_turns):  # [0, MAX_TURNS]
                     self.cmd_turns.data[i] += self.turns_resolution
-                elif (self.cmd_turns == DISABLE_TORQUE_REQUEST):
+                elif (self.cmd_turns.data[i] == DISABLE_TORQUE_REQUEST):
                     self.cmd_turns.data[i] = 0.0
+                elif (self.cmd_turns.data[i] == self.max_turns):
+                    rospy.loginfo("Maximum turns reached (%f) of the motor %d", self.cmd_turns.data[i], i)
+                    self.draw_motor_from_queue()
                 else:
-                    rospy.logerr("Invalid value of cmd_turn, i = %d",  i)
+                    rospy.logerr("Invalid value of cmd_turn = %f",  self.cmd_turns.data[i])
                     break
 
             # Request Torque Disable for the other motors
             else:
                 self.cmd_turns.data[i] = DISABLE_TORQUE_REQUEST
     
+    def draw_motor_from_queue(self):
+        if len(self.uncalibrated_motors) != 0:
+            rand_idx = np.random.randint(len(self.uncalibrated_motors))
+            # Add at the bottom the result
+            self.calibrated_motors.append(self.uncalibrated_motors[rand_idx])
+            # Then remove from the queue the motor
+            self.uncalibrated_motors.pop(rand_idx)
+        else:
+            rospy.signal_shutdown("Node terminated.")
+
     def publish_turns(self):
         self.pub_obj.publish(self.cmd_turns)
 
     def main_loop(self, event):
-        self.calibration_single_motor(0)
+        self.calibration_single_motor(self.calibrated_motors[-1])
         self.publish_turns()
